3dc3fe50eb57ea411aacb6743eb6514376f5eaca
RedJohn260 <damir.k93@gmail.com>
2022-07-20 18:55:22 +0200

    update to v1.0.0.3

--- YetAnotherYTDownloader/Form1.Designer.cs
+++ YetAnotherYTDownloader/Form1.Designer.cs
@@ -406,7 +406,7 @@
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Yet Another Youtube Downloader";
+            this.Text = "YAYD";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
